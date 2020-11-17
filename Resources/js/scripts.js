b822582caaeb4e1e1063fe7201394e2d381d08fa
Kapil Sharma <kapil0559@gmail.com>
2020-11-17 19:30:42 +0530

    Food ordering website

---
+++ Resources/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+    /* Scrolls on button */
+    $('.js--scroll-to-plans').click(function (){
+        $('html,body').animate({scrollTop: $(js--section-plans).offset().top}, 1000); 
+    })
+    
+    
+})
